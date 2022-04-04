916d831f235f3323fc846413d1ea931fedc29e05
Mark de Groot <mail@markdegroot.nl>
2022-04-04 10:51:43 +0200

    Add overlay for Xiaomi Poco X3 Pro

--- overlay.mk
+++ overlay.mk
@@ -216,6 +216,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-mipad4 \
 	treble-overlay-xiaomi-miplay \
 	treble-overlay-xiaomi-pocof1 \
+	treble-overlay-xiaomi-pocox3pro \
 	treble-overlay-xiaomi-redmi10x5g \
 	treble-overlay-xiaomi-redmi6 \
 	treble-overlay-xiaomi-redmi6a \
